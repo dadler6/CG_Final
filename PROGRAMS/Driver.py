bedc25a00eff38c091f75d0eb29dc1e45827c4ed
Dan Adler <daadler0309@gmail.com>
2015-12-09 21:19:14 -0500

    Brief changes to get it to run

--- PROGRAMS/Driver.py
+++ PROGRAMS/Driver.py
@@ -14,16 +14,16 @@ def _main():
 
     f = open('../data/reads.fa', 'r')
     reads_dict = init._process(f)
-    contigs = cs.consensus_sequence(reads_dict)
+    contigs = cs.run_consensus(reads_dict)
     likelihood = 0
     likelihood_new = 100
     while abs(abs(likelihood) - abs(likelihood_new)) > 1: # until likelihood converges
         likelihood = likelihood_new
         print likelihood
         reads_dict = rm.map_reads(reads_dict, contigs)
-        contigs = cs.consensus_sequence(reads_dict)
-        contigs = mc.merge_check_global(contigs,reads_dict) # how do we know if a merge has happened..do we need to know?
+        contigs = cs.run_consensus(reads_dict)
+        contigs, reads_dict = mc.run_merge(contigs,reads_dict) # how do we know if a merge has happened..do we need to know?
         likelihood_new = ll._likelihood(reads_dict,contigs)
-
-
+        print(reads_dict)
         assert likelihood_new-likelihood > 0 # likelihood is always increasing after a consensus sequence step
+_main()
--- PROGRAMS/Initialization.py
+++ PROGRAMS/Initialization.py
@@ -40,5 +40,3 @@ def _read_fa_file(f):
         if len(w) > 0:
             if i%2 == 1: reads.append(''.join(w))
     return reads
-
-_process()
