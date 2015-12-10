dfb734f7e8dc58bd29506883b6d49dfc4e6b15ac
farhan_damani <farhand7@gmail.com>
2015-12-10 15:04:01 -0500

    Added percent chance to contigs function in quality metrics script.

--- PROGRAMS/Driver.py
+++ PROGRAMS/Driver.py
@@ -14,18 +14,20 @@ import Quality_Metrics as qm
 NUM_ITERS = 20
 def _main():
 
-    f = open('../DATA/reads_25000.fa', 'r')
+    f = open('../DATA/0_wuhan_input.txt', 'r')
     reads_dict = init._process(f)
     contigs = cs.run_consensus(reads_dict)
     likelihood = 0
     likelihood_new = 0
-    contig_file = open('../SRC_OUTPUT/trial_two/contig.txt', 'w+')
-    ll_file = open('../SRC_OUTPUT/trial_two/likelihood.txt', 'w+')
+    contig_file = open('../SRC_OUTPUT/trial_three/contig.txt', 'w+')
+    ll_file = open('../SRC_OUTPUT/trial_three/likelihood.txt', 'w+')
     likelihood_list = []
     #while abs(abs(likelihood) - abs(likelihood_new)) > 1: # until likelihood converges
     for i in range(NUM_ITERS):
+
+        contig_file.write('%s\tstart\t' %(str(i)))
         for c in contigs:
-            contig_file.write('%s\tstart%s\t' %(str(i),str(c)))
+            contig_file.write('%s\t' %(str(c)))
         contig_file.write('\n')
         contig_file.flush()
         ll_file.write('%s\t%s\t%s\n' %(str(i), str(likelihood), str(len(contigs)))), ll_file.flush()
@@ -33,15 +35,16 @@ def _main():
         likelihood = likelihood_new
         reads_dict = rm.run(reads_dict, contigs)
         contigs = cs.run_consensus(reads_dict)
+        contig_file.write('%s\tmerge\t' %(str(i)))
         for c in contigs:
-            contig_file.write('%s\tmerge%s\t' %(str(i),str(c)))
-
+            contig_file.write('%s\t' %(str(c)))
+        contig_file.write('\n')
         contigs, reads_dict = mc.run_merge(contigs,reads_dict) # how do we know if a merge has happened..do we need to know?
         likelihood_new = ll._likelihood(reads_dict,contigs)
         #assert likelihood_new-likelihood > 0 # likelihood is always increasing after a consensus sequence step
 
     for c in contigs:
-        contig_file.write('1000\tend%s\t' %(str(c)))
+        contig_file.write('1000\tend\t%s\n' %(str(c)))
     ll_file.write('%s\t%s\t%s\n' %(str(NUM_ITERS), str(likelihood), str(len(contigs)))), ll_file.flush()
     likelihood_list.append(likelihood)
     qm._create_likelihood_figure(likelihood_list)
--- PROGRAMS/Quality_Metrics.py
+++ PROGRAMS/Quality_Metrics.py
@@ -3,6 +3,7 @@ __author__ = 'farhan_damani'
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def _create_likelihood_figure(ll_list):
     iters = []
     for i in range(len(ll_list)): iters.append(i)
@@ -23,7 +24,44 @@ def _process_likelihood_output_file(f):
 
 
 
-def _driver():
-    f = open('../SRC_OUTPUT/trial_one/likelihood.txt', 'r')
-    _create_likelihood_figure(_process_likelihood_output_file(f))
+def _percent_change_of_contigs():
+    '''
+        Evaluate percent chance of each contig over time steps.
+        Use edit distance to show this.
+    '''
 
+    f = open('../SRC_OUTPUT/trial_three/contig.txt', 'r')
+    c_prev = ''
+    contig_one_list = []
+    contig_two_list = []
+    contig_three_list = []
+    for l in f:
+        w = l.split('\t')
+        if len(w) > 0:
+           if str(w[1].strip())=='merge': continue
+           if str(w[1].strip()) =='end': continue
+           contig_one_list.append(str(w[2].strip())), contig_two_list.append(str(w[3].strip())), contig_three_list.append(str(w[4].strip()))
+
+    for i in xrange(0,len(contig_one_list)):
+        if i == 0: continue
+        print edDistDp(contig_one_list[i], contig_one_list[i-1]), edDistDp(contig_two_list[i], contig_two_list[i-1]), \
+            edDistDp(contig_three_list[i], contig_three_list[i-1])
+
+def edDistDp(x, y):
+    """ Calculate edit distance between sequences x and y using
+        matrix dynamic programming.  Return distance. """
+    D = np.zeros((len(x)+1, len(y)+1), dtype=int)
+    D[0, 1:] = range(1, len(y)+1)
+    D[1:, 0] = range(1, len(x)+1)
+    for i in range(1, len(x)+1):
+        for j in range(1, len(y)+1):
+            delt = 1 if x[i-1] != y[j-1] else 0
+            D[i, j] = min(D[i-1, j-1]+delt, D[i-1, j]+1, D[i, j-1]+1)
+    return D[len(x), len(y)]
+
+def _driver():
+    #f = open('../SRC_OUTPUT/trial_one/likelihood.txt', 'r')
+    #_create_likelihood_figure(_process_likelihood_output_file(f))
+    _percent_change_of_contigs()
+
+_driver()
